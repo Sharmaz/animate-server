5d25bb727eb51d53cff9ab09f3c4cebc6731e02a
Sharmaz <irae45@gmail.com>
2015-11-11 22:23:21 -0600

    Manejo de rutas, creamos modulo router, utilizamos el modulo de node st para servir archivos estaticos dentro de la carpeta public

--- server.js
+++ server.js
@@ -2,55 +2,16 @@
 
 const http = require('http')
 const fs = require('fs')
-const path = require('path')
+const router = require('./router')
 const port = process.env.PORT || 8080
 
 const server = http.createServer()
-server.on('request', onRequest)
+server.on('request', router)
 server.on('listening', onListening)
 
 server.listen(port)
 
-function onRequest(req, res) {
-	let uri = req.url
-	
-	if (uri.startsWith('/index') || uri === '/') {
-		return serveIndex(res)
-	}
 
-	if (uri === '/app.js') {
-		return serveApp(res)
-	}
-
-	res.statusCode = 404
-	res.end(`404 not found: ${uri}`)
-}
-
-function serveIndex (res) {
-	let index = path.join(__dirname, 'public', 'index.html')
-	
-	res.setHeader('Content-Type', 'text/html')
-	let rs = fs.createReadStream(index)
-	rs.pipe(res)
-	
-	rs.on('error', function(err) {
-		res.setHeader('Content-Type', 'text/html')
-		res.end(err.message)
-	})
-}
-
-function serveApp (res) {
-	let app = path.join(__dirname, 'public', 'app.js')
-	
-	res.setHeader('Content-Type', 'text/javascript')
-	let rs = fs.createReadStream(app)
-	rs.pipe(res)
-	
-	rs.on('error', function(err) {
-		res.setHeader('Content-Type', 'text/javascript')
-		res.end(err.message)
-	})
-}
 
 function onListening() {
 	console.log(`Servidor escuchando en el puerto: ${port}`)
