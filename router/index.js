4e5d5d6422cf1b985e3d39ac34e54f7006968a8a
Sharmaz <irae45@gmail.com>
2015-11-12 14:56:34 -0600

    Agregamos el modulo Course para el manejo de rutas (caracteristicas similares a express)

--- router/index.js
+++ router/index.js
@@ -1,18 +1,32 @@
 const st = require('st')
 const path = require('path')
+const course = require('course')
 
 const mount = st({
 	path: path.join(__dirname, '..', 'public'),
-	index: 'index.html'
+	index: 'index.html',
+	passthrough: true
 })
 
 function onRequest (req, res) {
 	mount(req, res, function(err) {
-		if (err) return res.end(err.message)
+		if (err) return fail(err, res)
 
-		res.statusCode = 404
-		res.end(`Not Found ${req.url}`)
+		router(req, res, function (err) {
+			if (err) return fail(err, res)
+
+			res.statusCode = 404
+			res.end(`Not Found ${req.url}`)
+		})
+
+
 	})
+}
+
+function fail (err, res) {
+	res.statusCode = 500
+	res.setHeader('Content-Type', 'text/plain')
+	res.end(err.message)
 }
 
 module.exports = onRequest
