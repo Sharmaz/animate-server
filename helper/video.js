530e5c5a97784b5d9bdf011f3491c53fdb6cfcb6
Sharmaz <irae45@gmail.com>
2015-11-13 01:39:40 -0600

    Usamos el modulo data-uri-to-buffer que convierte data-uri a buffer(binarios), se decodificaron las imagenes y se guardaron en disco duro, las imagenes se les asignó una secuencia con async.eachSeris y un id unico con el modulo uuid

--- helper/video.js
+++ helper/video.js
@@ -2,9 +2,17 @@
 
 const EventEmitter = require('events').EventEmitter
 const async = require('async')
+const dataURIBuffer = require('data-uri-to-buffer')
+const fs = require('fs')
+const os = require('os')
+const path = require('path')
+const uuid = require('uuid')
 
 module.exports = function (images) {
 	let events = new EventEmitter()
+	let count = 0
+	let baseName = uuid.v4()
+	let tmpDir = os.tmpDir()
 
 	async.series([
 		decodeImages,
@@ -13,8 +21,19 @@ module.exports = function (images) {
 		cleanup
 		], converFinished)
 
-	function decodeImages(done) {
-		done()
+	function decodeImages (done) {
+		async.eachSeries(images, decodeImage, done)
+	}
+
+	function decodeImage (image, done) {
+		let fileName = `${baseName}-${count++}.jpg`
+		let buffer = dataURIBuffer(image)
+		let ws = fs.createWriteStream(path.join(tmpDir, fileName))
+
+		ws.on('error', done)
+			.end(buffer, done)
+
+		events.emit('log', `Converting $(fileName`)
 	}
 
 	function createVideo (done) {
