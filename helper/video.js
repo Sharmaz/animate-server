4c59e451c25f211e6d4ecfec8f17f05467696bd5
Sharmaz <irae45@gmail.com>
2015-11-13 00:51:46 -0600

    Creamos el workflow basico para la conversión de video usando el modulo async que nos permite ejecutar funciones en serie

--- helper/video.js
+++ helper/video.js
@@ -1,13 +1,39 @@
 'use strict'
 
 const EventEmitter = require('events').EventEmitter
+const async = require('async')
 
 module.exports = function (images) {
 	let events = new EventEmitter()
 
-	setTimeout(function () {
+	async.series([
+		decodeImages,
+		createVideo,
+		encodeVideo,
+		cleanup
+		], converFinished)
+
+	function decodeImages(done) {
+		done()
+	}
+
+	function createVideo (done) {
+		done()
+	}
+
+	function encodeVideo (done) {
+		done()
+	}
+
+	function cleanup (done) {
+		done()
+	}
+
+	function converFinished (err) {
+		setTimeout(function () {
 		events.emit('video', 'this will be the encoded video')
-	}, 1000)
+		}, 1000)
+	}
 
 	return events
 }
--- router/index.js
+++ router/index.js
@@ -18,12 +18,20 @@ router.post('/process', function(req, res) {
 	jsonBody(req, res, { limit: 3 * 1024 * 1024 }, function (err, body) {
 		if (err) return fail(err, res)
 
-		let converter = helper.convertVideo(body.images)
+		if (Array.isArray(body.images)) {
 
-		converter.on('video', function (video) {
+			let converter = helper.convertVideo(body.images)
+
+			converter.on('video', function (video) {
 			res.setHeader('Content-Type', 'application/json')
 			res.end(JSON.stringify({ video: video }))
 		})	
+
+		} else {
+			res.statusCode = 500
+			res.end(JSON.stringify({ error: 'parameter `images` is required'}))
+		}
+
 
 	})
 
